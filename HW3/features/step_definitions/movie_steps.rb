3c7946676c0d816b5957e64a322c707e8452580e
Plínio Mayer <pctmayer@gmail.com>
2022-03-22 20:10:43 -0300

    Part 3.

--- HW3/features/step_definitions/movie_steps.rb
+++ HW3/features/step_definitions/movie_steps.rb
@@ -19,3 +19,10 @@ end
 Then /I should see all the movies/ do
   Movie.all.count == 10
 end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  page.body
+  e1_pos = page.body.index(e1)
+  e2_pos = page.body.index(e2)
+  e1_pos <= e2_pos
+end
