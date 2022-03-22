84586437ab3c5e73a87e0d67bc53bbebc81ce2b9
Plínio Mayer <pctmayer@gmail.com>
2022-03-22 20:07:45 -0300

    Part 2.

--- HW3/features/step_definitions/movie_steps.rb
+++ HW3/features/step_definitions/movie_steps.rb
@@ -3,3 +3,19 @@ Given /the following movies exist/ do |movies_table|
     Movie.create!(movie)
   end
 end
+
+When /I check the following ratings: (.*)/ do |rating_list|
+  rating_list.split(%r{,\s*}).each do |rating|
+    check('ratings_' + rating)
+  end
+end
+
+When /I uncheck the following ratings: (.*)/ do |rating_list|
+  rating_list.split(%r{,\s*}).each do |rating|
+    uncheck('ratings_' + rating)
+  end
+end
+
+Then /I should see all the movies/ do
+  Movie.all.count == 10
+end
